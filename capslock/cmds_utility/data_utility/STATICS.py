82444dc8e8a21e812834a82c62ddc06518b2c264
GalacticInferno <cody.beaty2@hotmail.com>
2017-07-31 13:42:33 -0700

    updated version

--- capslock/cmds_utility/data_utility/STATICS.py
+++ capslock/cmds_utility/data_utility/STATICS.py
@@ -2,7 +2,7 @@ NAME = "CAPSLOCK"
 AUTHOR = "Cody Beaty, 2017"
 
 PREFIX = "!"
-VERSION = "1.0.1"
+VERSION = "2.0.0"
 
 INVITE_BOT = 'https://discordapp.com/api/oauth2/authorize?client_id=' \
     '304011285174157322&scope=bot&permissions=0'
